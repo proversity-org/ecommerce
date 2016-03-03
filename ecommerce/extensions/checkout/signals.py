0e1436a584995b86b606d114ad1ac26deca74cb1
Joyce Zhu <jzhu@edx.org>
2016-03-03 12:40:41 +0000

    Implement new receipt page (moved from LMS)
    Replaces LMS commerce receipt page.
    Shows voucher codes applied, shipping/billing addresses, and payment methods
    used.
    Introduces otto_receipt_page waffle switch.
    Serializes Object payment processor, vouchers, and shipping address.
    
    SOL-1953

--- ecommerce/extensions/checkout/signals.py
+++ ecommerce/extensions/checkout/signals.py
@@ -6,7 +6,7 @@ from oscar.core.loading import get_class
 from threadlocals import threadlocals
 import waffle
 
-from ecommerce.core.url_utils import get_lms_url
+from ecommerce.core.url_utils import get_ecommerce_url, get_lms_url
 from ecommerce.courses.utils import mode_for_seat
 from ecommerce.extensions.analytics.utils import is_segment_configured, parse_tracking_context, silence_exceptions
 from ecommerce.extensions.checkout.utils import get_provider_data
@@ -76,15 +76,19 @@ def send_course_purchase_email(sender, order=None, **kwargs):  # pylint: disable
                 return
             elif product.get_product_class().name == 'Seat':
                 provider_data = get_provider_data(provider_id)
+                if waffle.switch_is_active('otto_receipt_page'):
+                    receipt_page_url = get_ecommerce_url(
+                        '{}?order_number={}'.format(settings.RECEIPT_PAGE_PATH, order.number)
+                    )
+                else:
+                    receipt_page_url = get_lms_url('{}?orderNum={}'.format('/commerce/checkout/receipt', order.number))
                 if provider_data:
                     send_notification(
                         order.user,
                         'CREDIT_RECEIPT',
                         {
                             'course_title': product.title,
-                            'receipt_page_url': get_lms_url(
-                                '{}?orderNum={}'.format(settings.RECEIPT_PAGE_PATH, order.number)
-                            ),
+                            'receipt_page_url': receipt_page_url,
                             'credit_hours': product.attr.credit_hours,
                             'credit_provider': provider_data['display_name'],
                         },
--- ecommerce/extensions/checkout/tests/test_signals.py
+++ ecommerce/extensions/checkout/tests/test_signals.py
@@ -8,7 +8,7 @@ from oscar.test.newfactories import BasketFactory
 from threadlocals.threadlocals import get_current_request
 
 from ecommerce.core.tests import toggle_switch
-from ecommerce.core.url_utils import get_lms_url
+from ecommerce.core.url_utils import get_ecommerce_url, get_lms_url
 from ecommerce.courses.models import Course
 from ecommerce.extensions.catalogue.tests.mixins import CourseCatalogTestMixin
 from ecommerce.extensions.checkout.signals import send_course_purchase_email
@@ -25,6 +25,7 @@ class SignalTests(CourseCatalogTestMixin, TestCase):
         self.request.user = self.user
         self.site_configuration = SiteConfigurationFactory(partner__name='Tester', from_email='from@example.com')
         self.request.site = self.site_configuration.site
+        toggle_switch('otto_receipt_page', True)
 
     @httpretty.activate
     def test_post_checkout_callback(self):
@@ -72,6 +73,56 @@ class SignalTests(CourseCatalogTestMixin, TestCase):
                 credit_hours=2,
                 credit_provider='Hogwarts',
                 platform_name=get_current_request().site.name,
-                receipt_url=get_lms_url('{}?orderNum={}'.format(settings.RECEIPT_PAGE_PATH, order.number))
+                receipt_url=get_ecommerce_url('{}?order_number={}'.format(settings.RECEIPT_PAGE_PATH, order.number))
+            )
+        )
+
+    @httpretty.activate
+    def test_disable_switch_reverts_to_lms_receipt_page(self):
+        """
+        Ensures that disabling the otto_receipt_page switch results in using the LMS receipt url.
+        """
+        httpretty.register_uri(
+            httpretty.GET, get_lms_url('api/credit/v1/providers/ASU'),
+            body='{"display_name": "Hogwarts"}',
+            content_type="application/json"
+        )
+        toggle_switch('ENABLE_NOTIFICATIONS', True)
+        toggle_switch('otto_receipt_page', False)
+        course = Course.objects.create(id='edX/DemoX/Demo_Course', name='Demo Course')
+        seat = course.create_or_update_seat('credit', False, 50, self.partner, 'ASU', None, 2)
+
+        basket = BasketFactory()
+        basket.add_product(seat, 1)
+        order = factories.create_order(number=1, basket=basket, user=self.user)
+        with mock.patch('threadlocals.threadlocals.get_current_request') as mock_gcr:
+            mock_gcr.return_value = self.request
+            send_course_purchase_email(None, order=order)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].from_email, self.site_configuration.from_email)
+        self.assertEqual(mail.outbox[0].subject, 'Order Receipt')
+        self.assertEqual(
+            mail.outbox[0].body,
+            '\nPayment confirmation for: {course_title}'
+            '\n\nDear {full_name},'
+            '\n\nThank you for purchasing {credit_hours} credit hours from {credit_provider} for {course_title}. '
+            'A charge will appear on your credit or debit card statement with a company name of "{platform_name}".'
+            '\n\nTo receive your course credit, you must also request credit at the {credit_provider} website. '
+            'For a link to request credit from {credit_provider}, or to see the status of your credit request, '
+            'go to your {platform_name} dashboard.'
+            '\n\nTo explore other credit-eligible courses, visit the {platform_name} website. '
+            'We add new courses frequently!'
+            '\n\nTo view your payment information, visit the following website.'
+            '\n{receipt_url}'
+            '\n\nThank you. We hope you enjoyed your course!'
+            '\nThe {platform_name} team'
+            '\n\nYou received this message because you purchased credit hours for {course_title}, '
+            'an {platform_name} course.\n'.format(
+                course_title=order.lines.first().product.title,
+                full_name=self.user.get_full_name(),
+                credit_hours=2,
+                credit_provider='Hogwarts',
+                platform_name=get_current_request().site.name,
+                receipt_url=get_lms_url('{}?orderNum={}'.format('/commerce/checkout/receipt', order.number))
             )
         )
